456020814b6163f700b609fffdd48d08eae4af16
Jeffery Ho <chiachun2491@gmail.com>
2022-10-20 11:58:27 +0800

    fix: update new version (2.1.5) apps url
    - temporarily use github.io domain

--- oral.js
+++ oral.js
@@ -1,4 +1,4 @@
-const gas = "https://script.google.com/macros/s/AKfycbzNxrePxHdsLXgQIj3OiyttnR_90-6EsNznnCfobENKWEid-ZALkzuoxbFY6LHm_c2xjA/exec";
+const gas = "https://script.google.com/macros/s/AKfycby3DisxyDR89gCyrLKolCaj6TGVPpdhrDTyGFHaLqyRQTOuRf9Plzc4V_YnkXYmp8cI4g/exec";
 
 $(document).ready(function () {
     let urlParams = new URLSearchParams(window.location.search);
